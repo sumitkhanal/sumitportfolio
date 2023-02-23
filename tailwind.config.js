297ce28d391464e80712523625096f426a0d12aa
Sumit Khanal <sumit.khanal80@gmail.com>
2023-02-23 21:42:32 +0545

    first deploy

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*,{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily:{
+        burtons:"burtons",
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
