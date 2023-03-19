d6c66da09867f17e5cb82bc73cb4f03d21573447
sumitkhanal <55523893+sumitkhanal@users.noreply.github.com>
2023-03-19 17:12:27 +0545

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,7 @@ export default function Home() {
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="https://sumitkhanal.github.io/sumitportfolio/SumitKhanal_CV.pdf" target= "_blank" rel="noreferrer">
+                  href="/SumitKhanal_CV.pdf" target= "_blank" rel="noreferrer">
                   Resume
                 </a>
               </li>
